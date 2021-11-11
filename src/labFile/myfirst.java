5a056150b4a86c63372e306bd8237d4bb6d02901
himanshu06d <hd156681@gmail.com>
2021-11-11 22:14:29 +0530

    java code

---
+++ src/labFile/myfirst.java
@@ -0,0 +1,11 @@
+package labFile;
+
+public class myfirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the first line of code");
+
+	}
+
+}
